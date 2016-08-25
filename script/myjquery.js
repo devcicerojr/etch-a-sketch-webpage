5f0185eb8157ded0e0d3ad3311bb852a378e8423
devcicerojr <devcicerojr@gmail.com>
2016-08-25 09:15:25 -0300

    Update myjquery.js

--- script/myjquery.js
+++ script/myjquery.js
@@ -1,14 +1,9 @@
-var rowNumber;
-var colNumber;
-var cellWidth;
-var cellHeight;
+var rowNumber = 16;
+var colNumber = 16;
+var cellWidth = ($("div#gridArea").width()) / colNumber;
+var cellHeight = ($("div#gridArea").height()) / rowNumber;
 
 $(document).ready(function() {
-  rowNumber = 16;
-  colNumber = 16;
-  cellWidth = ($("div#gridArea").width()) / colNumber;
-  cellHeight = ($("div#gridArea").height()) / rowNumber;
-
   CreateTable();
 
   $("button").click(function() {
